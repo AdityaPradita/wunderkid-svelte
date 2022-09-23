81e9872a25b4f2f893e81f810ac4b156c4d85188
Aditya Pradita <aditya.pradita@quipper.com>
2022-09-23 15:58:06 +0700

    Add API Call for League Table

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import LeagueTable from "./components/LeagueTable.svelte";
-  import MainComponent from "./components/MainComponent.svelte";
 </script>
 
 <main>
--- src/components/LeagueTable.svelte
+++ src/components/LeagueTable.svelte
@@ -1,3 +1,24 @@
+<script>
+  import { onMount } from "svelte";
+  import axios from "axios";
+
+  onMount(async () => {
+    fetch("https://api.football-data.org/v2/competitions/BL1/standings", {
+      headers: new Headers({
+        "X-Auth-Token": import.meta.env.VITE_API_KEY,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        return [];
+      });
+  });
+</script>
+
 <div>
   <p>Hello world!</p>
 </div>
